4d0dac001b4cc46da7a6d1de8546fc7d7a2ac4eb
kth528 <spmm528@gmail.com>
2022-07-08 16:35:40 +0900

    a

---
+++ src/java_personal/tt/aaaa.java
@@ -0,0 +1,14 @@
+package java_personal.tt;
+
+public class aaaa {
+	public static void main(String[] args) {
+		
+		char a,b;
+			Scanner scan = new Scanner(System.in);
+			a = scan.next().charAt(0);
+			b = scan.next().charAt(0);
+			System.out.printf("%c %c", b, a);
+	}
+}
+
+/* https://jarin-gobi.tistory.com/255 */
