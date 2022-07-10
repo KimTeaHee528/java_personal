8732388b62bd1d4cf31554d22befe0a5106ad6e4
home <spmm528@gmail.com>
2022-07-10 20:16:51 +0900

    a

--- src/ch09/SubjectTest.java
+++ src/ch09/SubjectTest.java
@@ -5,9 +5,8 @@ public class SubjectTest {
 	public static void main(String[] args) {
 
 		Student studentKimmm = new Student(01, "kimmm");
-		studentKimmm.setKoreaSubject("국어", 95);
-		studentKimmm.setMathSubject("수학", 100);
 		studentKimmm.korea.score = 80;
+		studentKimmm.setMathSubject("수학", 100);
 		studentKimmm.showScoreInFo();
 	}
 
