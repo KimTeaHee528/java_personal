d57a1d3b82c1963a43843ce1338240026355893e
kth <spmm528@gmail.com>
2022-07-25 09:48:34 +0900

    a

--- src/ch04/Student.java
+++ src/ch04/Student.java
@@ -9,7 +9,7 @@ public class Student {
 		System.out.println( studentID + "학번 학생의 이름은" + studentName +"이고, 주소는" + address +"입니다.");
 	}
 	public String getStudentName() {
-		return studentName;
+		return studentName; 
 	}
 	public void setStudentName(String name) {
 		studentName = name;
